3548359c13cdd22bc5642d3d223676c301d1ef46
wucuida <da727438548@qq.com>
2017-07-05 23:24:58 +0800

    修改分页样式，模拟请求告警数据

--- src/components/alarm.vue
+++ src/components/alarm.vue
@@ -5,18 +5,18 @@
 			<tool-bar></tool-bar>
 		</el-col>
 	</el-row>
-	<el-row>
+	<el-row v-loading="loading"
+		element-loading-text="加载中...">
 		<el-col>
 			<el-table :data="tableData" style="width: 100%" :height="height">
-	 			<el-table-column v-for="col in columns" 
+	 			<el-table-column v-for="col in columns" sortable
 	 			:prop="col.name" :label="col.content"> 			
 	 			</el-table-column>
-    </el-table>
+    	</el-table>
 		</el-col>
 	</el-row>
-	<el-row style="position:fixed">
-		<el-col>
-			<el-pagination
+	<div style="position:fixed;width:100%;text-align:center">
+		<el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="currentPage"
@@ -25,14 +25,14 @@
       :total="100"
       layout="total, sizes, prev, pager, next, jumper">
     </el-pagination>
-		</el-col>
-	</el-row>
+
+	</div>
 </div>
 </template>
 <script type="text/javascript">
 let mydata = [
 	{region: "东城区", "siteNumber": "321", "siteName": "配电室1", action: 1, data: "1234567889", retransmission: 1},
-	{region: "东城区", "siteNumber": "321", "siteName": "配电室1", action: 1, data: "1234567889", retransmission: 1},
+	{region: "东城区", "siteNumber": "321", "siteName": "配电室1", action: 1, data: "1234567889", retransmission: 0},
 	{region: "东城区", "siteNumber": "321", "siteName": "配电室1", action: 1, data: "1234567889", retransmission: 1},
 	{region: "东城区", "siteNumber": "321", "siteName": "配电室1", action: 1, data: "1234567889", retransmission: 1},
 	{region: "东城区", "siteNumber": "321", "siteName": "配电室1", action: 1, data: "1234567889", retransmission: 1},
@@ -66,8 +66,9 @@ export default {
 		]
 		return {
 			columns: columns,
-			tableData: mydata,
-			currentPage: 0
+			tableData: [],
+			currentPage: 0,
+			loading: true
 		}
 	},
 	components: {
@@ -84,7 +85,24 @@ export default {
 		},
 		handleCurrentChange() {
 
+		},
+		getData() {
+			let promise = new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve(mydata)
+				}, 1000)
+			})
+			return promise
 		}
+	},
+	created() {
+		this.loading = true
+		this.getData().then(data => {
+			this.tableData = data
+			this.$nextTick(() => {
+				this.loading = false
+			})
+		})
 	}
 }
 </script>
